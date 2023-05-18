fbcedea96367a52c7fd6dc33173f59c4172581c7
mrp4sten <mauricio.pasten.martinez@gmail.com>
2023-05-18 16:52:17 -0600

    Adding Alert component

--- js/components/add-todo.js
+++ js/components/add-todo.js
@@ -1,18 +1,20 @@
+import Alert from './alert.js';
+
 export default class AddTodo {
   constructor() {
     this.title = document.getElementById('title');
     this.description = document.getElementById('description');
     this.addBtn = document.getElementById('add');
+    this.alert = new Alert('alert');
   }
 
   onClick(callback) {
     this.addBtn.addEventListener('click', () => {
       if (this.title.value === '' || this.description.value === '') {
-        // alert.classList.remove('d-none');
-        // alert.innerText = 'Title & Description couldnt be empty';
-        // return;
+        this.alert.show('Title & Description couldnt be empty');
         console.error('ERROR');
       } else {
+        this.alert.hide();
         callback(this.title.value, this.description.value);
       }
     });
